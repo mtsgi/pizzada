79874986fac534f72aae13b2969b9bf20eeefcdd
mtsgi <oq@live.jp>
2019-03-09 19:05:28 +0900

    UIの整備

--- pizzada.js
+++ pizzada.js
@@ -1,4 +1,5 @@
 var time = 0, point = 0;
+var targets = ["pizza", "class", "constructor", "document", "location", "var", "const", "getElementById", "function()"];
 
 class Pizzada{
     constructor(string){
@@ -10,9 +11,11 @@ class Pizzada{
         $("#"+str).show();
     }
     static pizza(){
+        //END
         if( point == 3 ){
             Pizzada.section("pizzada-result");
             $("#pizzada-time").text(time);
+            $("#pizzada-times").text("");
             point = 0;
             return time;
         }
@@ -20,18 +23,19 @@ class Pizzada{
         Pizzada.section("pizzada-game");
         //START
         let start = Date.now();
-        p = new Pizzada("constructor");
+        let ran = Math.floor(Math.random() * Math.floor(targets.length));
+        p = new Pizzada( targets[ran] );
         $("#pizzada-target").text( p.target );
         $("#pizzada-enter").text("");
         //BIND
         $(document).on("keydown", (event) => {
             if( p.target[p.count] == event.key ){
-                $("#pizzada-enter").append( event.key.toLowerCase() );
+                $("#pizzada-enter").append( event.key );
                 p.count++;
             }
             if( p.count == p.target.length ){
                 time += Number( Date.now() - start );
-                alert("time:" + time );
+                $("#pizzada-times").html("<div>"+time+"</div>");
                 point++;
                 $(document).off();
                 Pizzada.pizza();
