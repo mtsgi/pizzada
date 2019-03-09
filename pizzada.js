7b750214937b1a3a61fdc58a915919c396f69b7a
mtsgi <oq@live.jp>
2019-03-09 18:42:11 +0900

    順番にpizzaを実行

--- pizzada.js
+++ pizzada.js
@@ -1,27 +1,40 @@
+var time = 0, point = 0;
+
 class Pizzada{
     constructor(string){
         this.target = string;
+        this.count = 0;
     }
     static section( str ){
         $("section").hide();
         $("#"+str).show();
     }
-    static init(){
-        var pizza;
+    static pizza(){
+        if( point == 3 ){
+            Pizzada.section("pizzada-result");
+            $("#pizzada-time").text(time);
+            point = 0;
+            return time;
+        }
+        var p;
         Pizzada.section("pizzada-game");
+        //START
         let start = Date.now();
-        let count = 0, point = 0;
-        pizza = new Pizzada("constructor");
-        $("#pizzada-target").text( pizza.target );
-        //bind
+        p = new Pizzada("constructor");
+        $("#pizzada-target").text( p.target );
+        $("#pizzada-enter").text("");
+        //BIND
         $(document).on("keydown", (event) => {
-            if( pizza.target[count] == event.key ){
+            if( p.target[p.count] == event.key ){
                 $("#pizzada-enter").append( event.key.toLowerCase() );
-                count++;
+                p.count++;
             }
-            if( count == pizza.target.length ){
-                alert("time:" + Number( Date.now() - start ) );
-                return;
+            if( p.count == p.target.length ){
+                time += Number( Date.now() - start );
+                alert("time:" + time );
+                point++;
+                $(document).off();
+                Pizzada.pizza();
             }
         })
     }
