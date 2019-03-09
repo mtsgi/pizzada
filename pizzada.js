6d144c72796d0f64c285ad28065937441f59c958
mtsgi <oq@live.jp>
2019-03-09 18:19:27 +0900

    入力部分の実装

--- pizzada.js
+++ pizzada.js
@@ -1,13 +1,29 @@
 class Pizzada{
-    constructor(){
-        this.pizza = 0;
+    constructor(string){
+        this.target = string;
     }
     static section( str ){
         $("section").hide();
         $("#"+str).show();
     }
     static init(){
-        return new Date();
+        var pizza;
+        Pizzada.section("pizzada-game");
+        let start = Date.now();
+        let count = 0, point = 0;
+        pizza = new Pizzada("constructor");
+        $("#pizzada-target").text( pizza.target );
+        //bind
+        $(document).on("keydown", (event) => {
+            if( pizza.target[count] == event.key ){
+                $("#pizzada-enter").append( event.key.toLowerCase() );
+                count++;
+            }
+            if( count == pizza.target.length ){
+                alert("time:" + Number( Date.now() - start ) );
+                return;
+            }
+        })
     }
 }
 $(function(){
